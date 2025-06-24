a38e67100cdd40de48226d0c25523d25cf3040a3
Mohanad <mohanad.aldaghestani@gmail.com>
2025-06-24 13:22:22 +0200

    It seems that i forgot the connectionstring after all

--- Infrastructure/Database/RealDatabaseFactory.cs
+++ Infrastructure/Database/RealDatabaseFactory.cs
@@ -19,7 +19,7 @@ namespace Infrastructure.Database
 
             var optionsBuilder = new DbContextOptionsBuilder<RealDatabase>();
 
-            optionsBuilder.UseNpgsql("Host=aws-0-eu-north-1.pooler.supabase.com;Port=5432;Database=postgres;Username=postgres.ympfphskbjblatliknnf;Password=uBPoZ51UQEvvBBsg;SslMode=Require;Trust Server Certificate=true");
+            optionsBuilder.UseNpgsql(connectionString);
 
             return new RealDatabase(optionsBuilder.Options);
         }
